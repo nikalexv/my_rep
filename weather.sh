3b4112b6e154ab4eaa2af05f4c240f55f5c6696a
nikalexv <nikalexv.alexey92@hotmail.com>
2021-05-20 13:26:43 +0300

    update

--- weather.sh
+++ weather.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#Проверка ввода параметра скрипта 
+#city input validation
 if [ -n "$1" ]
 then
-#вывод данных во временный файл
- curl -s wttr.in/$1?format=j1 > test.$$
-#c помощью библиотеки jq находим необходимые данные в json   
+# json stdout in tempory file 
+curl -s wttr.in/$1?format=j1 > test.$$
+#jq parser 
  temperature=`jq -r '.current_condition[0].FeelsLikeC' test.$$`
  humidity=`jq -r '.current_condition[0].humidity' test.$$`
  echo "Its $temperature degrees Celsius outside with $humidity% of humidity"
-#удаляем временный файл
+#delete tempory file 
  rm -rf test.$$
 else
  echo "No parameters found"
